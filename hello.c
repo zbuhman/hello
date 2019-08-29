66b1a6d50b8001714f46278ee264aadcc233b6ea
Zach Buhman <zbuhman2@huskers.unl.edu>
2019-08-29 11:38:12 -0500

    Create hello.c
    Added print for major

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Zach Buhman
+ * Date: 2019/8/29
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Zach Buhman\n");
+
+  printf("Actuarial Science\n");
+
+  return 0;
+}
